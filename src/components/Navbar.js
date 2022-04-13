4a2e64ff3787275e04b51fd281cd64041bd6d3f3
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2022-04-13 11:46:29 +0100

    Meme generator app
    Meme generator app created using React

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react"
+import Trollface from "../images/trollface.jpg"
 
-
-export default function Navbar()  {
+export default function Navbar() {
     return (
-        <nav>
-            <h3 className="nav--logo_text">ReactFacts </h3>
-            <h4 className="nav--title">React Course Project 1</h4>
-            
-        </nav>
-    )
-}
+        <header className = "header">
+        <img className = "header--image" src={Trollface} />
+        <h2 className = "header--title ">Meme Generator </h2>
+        <h4 className = "header--project"> React Course  - Project</h4>
+        </header>
+        
+    )}
+
